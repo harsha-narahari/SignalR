3616baccf51eac63db71d5921207af58ebac306f
Harsha Narahari <harshan@cos.net.au>
2017-02-01 15:51:04 +1100

    inital

---
+++ TimeHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace SignalRBroadcastSample
+{
+    public class TimeHub : Hub
+    {
+        private readonly TimerHelper _timerHelper;
+
+        public TimeHub() : this(TimerHelper.Instance) { }
+
+        public TimeHub(TimerHelper stockTicker)
+        {
+            _timerHelper = stockTicker;
+        }
+
+        public void GetTime()
+        {
+            Clients.All.updateTime(DateTime.Now.ToLongTimeString());
+        }
+    }    
+}
---
+++ TimerHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Threading;
+
+namespace SignalRBroadcastSample
+{
+    public class TimerHelper
+    {
+        private Timer _timeUpdater;
+
+        private readonly static Lazy<TimerHelper> _instance = new Lazy<TimerHelper>(() => new TimerHelper(GlobalHost.ConnectionManager.GetHubContext<TimeHub>().Clients));
+
+        public static TimerHelper Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        private IHubConnectionContext<dynamic> Clients
+        {
+            get;
+            set;
+        }
+
+        private TimerHelper(IHubConnectionContext<dynamic> clients)
+        {
+            Clients = clients;
+            if (_timeUpdater == null)
+            {
+                _timeUpdater = new Timer(UpdateTime, null, 5000, 5000);
+            }
+        }
+
+        public void UpdateTime(object state)
+        {
+            Clients.All.updateTime(DateTime.Now.ToLongTimeString());
+        }
+    }
+}
